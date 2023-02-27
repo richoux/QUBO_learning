184426d28c09aad0f08df15263dca45ca0a7ec4e
Florian Richoux <florian.richoux@polytechnique.edu>
2023-02-27 14:20:38 +0900

    Cleaning spaces

--- run_xp2.sh
+++ run_xp2.sh
@@ -8,38 +8,41 @@ echo ""
 echo "AllDiff"
 for i in {1..$LOOP}
 do
-		$EXE -f spaces/incomplete/alldiff-15_15_100.txt -p -r tmp --benchmark 2> /dev/null 1> /dev/null
-		$EXE -f spaces/incomplete/alldiff-15_15_100000.txt -c tmp --benchmark >> out_error
+		$EXE -f spaces/incomplete/alldiff-12_12_100.txt -p -r tmp --benchmark 2> /dev/null 1> /dev/null
+		$EXE -f spaces/test/alldiff-30_30.txt -c tmp --benchmark >> out_error
 done
 SUM=`awk -v sum=0 '{sum += $4} END {print sum}' out_error`
+echo "Total sum of errors: $SUM"
 MEAN=$(echo "scale=2; $SUM/$LOOP" | bc)
-echo "Mean errors: $MEAN"
+echo "Mean error: $MEAN"
 
 rm tmp out_error
 
-echo ""
-echo "Ordered"
-for i in {1..$LOOP}
-do
-		$EXE -f spaces/incomplete/ordered-12_18_100.txt -p -r tmp --benchmark 2> /dev/null 1> /dev/null
-		$EXE -f spaces/incomplete/ordered-12_18_10000.txt -c tmp --benchmark >> out_error
-done
-SUM=`awk -v sum=0 '{sum += $4} END {print sum}' out_error`
-MEAN=$(echo "scale=2; $SUM/$LOOP" | bc)
-echo "Mean errors: $MEAN"
+# echo ""
+# echo "Ordered"
+# for i in {1..$LOOP}
+# do
+# 		$EXE -f spaces/incomplete/ordered-12_12_100.txt -p -r tmp --benchmark 2> /dev/null 1> /dev/null
+# 		$EXE -f spaces/test/ordered-30_30.txt -c tmp --benchmark >> out_error
+# done
+# SUM=`awk -v sum=0 '{sum += $4} END {print sum}' out_error`
+# echo "Total sum of errors: $SUM"
+# MEAN=$(echo "scale=2; $SUM/$LOOP" | bc)
+# echo "Mean error: $MEAN"
 
-rm tmp out_error
+# rm tmp out_error
 
 echo ""
 echo "LinearSum"
 for i in {1..$LOOP}
 do
 		$EXE -f spaces/incomplete/linear_equation-12_12_72_100.txt -p -r tmp --benchmark 2> /dev/null 1> /dev/null
-		$EXE -f spaces/incomplete/linear_equation-12_12_72_10000.txt -c tmp --benchmark >> out_error
+		$EXE -f spaces/test/linear_equation-30_30_600.txt -c tmp --benchmark >> out_error
 done
 SUM=`awk -v sum=0 '{sum += $4} END {print sum}' out_error`
+echo "Total sum of errors: $SUM"
 MEAN=$(echo "scale=2; $SUM/$LOOP" | bc)
-echo "Mean errors: $MEAN"
+echo "Mean error: $MEAN"
 
 rm tmp out_error
 
@@ -47,12 +50,13 @@ echo ""
 echo "NoOverlap"
 for i in {1..$LOOP}
 do
-		$EXE -f spaces/incomplete/no_overlap_1D-12_48_3_100.txt -p -r tmp --benchmark 2> /dev/null 1> /dev/null
-		$EXE -f spaces/incomplete/no_overlap_1D-12_48_3_10000.txt -c tmp --benchmark >> out_error
+		$EXE -f spaces/incomplete/no_overlap_1D-8_35_3_100.txt -p -r tmp --benchmark 2> /dev/null 1> /dev/null
+		$EXE -f spaces/test/no_overlap_1D-20_160_6.txt -c tmp --benchmark >> out_error
 done
 SUM=`awk -v sum=0 '{sum += $4} END {print sum}' out_error`
+echo "Total sum of errors: $SUM"
 MEAN=$(echo "scale=2; $SUM/$LOOP" | bc)
-echo "Mean errors: $MEAN"
+echo "Mean error: $MEAN"
 
 rm tmp out_error
 
@@ -61,10 +65,11 @@ rm tmp out_error
 # for i in {1..$LOOP}
 # do
 # 		$EXE -f spaces/incomplete/channel-12_12_100.txt -p -r tmp --benchmark 2> /dev/null 1> /dev/null
-# 		$EXE -f spaces/incomplete/channel-12_12_10000.txt -c tmp --benchmark >> out_error
+# 		$EXE -f spaces/test/channel-30_30.txt -c tmp --benchmark >> out_error
 # done
 # SUM=`awk -v sum=0 '{sum += $4} END {print sum}' out_error`
+# echo "Total sum of errors: $SUM"
 # MEAN=$(echo "scale=2; $SUM/$LOOP" | bc)
-# echo "Mean errors: $MEAN"
+# echo "Mean error: $MEAN"
 
 # rm tmp out_error
