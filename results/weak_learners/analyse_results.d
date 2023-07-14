bb740ab25b8da92310599c6d35678dad30dcc5f3
Florian Richoux <florian.richoux@polytechnique.edu>
2023-07-14 10:31:30 +0900

    Update weak learners analyze script

--- results/weak_learners/analyse_results.d
+++ results/weak_learners/analyse_results.d
@@ -38,31 +38,19 @@ int main( string[] args )
 		return 1;
 	}
 
-	auto filename_majority = args[1] ~ "-majority";
-	auto filename_mean = args[1] ~ "-mean";
 	auto filename_test_error_majority = args[1] ~ "-test_error_majority";
 	auto filename_test_error_mean = args[1] ~ "-test_error_mean";
 	auto filename_training = args[1] ~ "-training";
 	
-	auto file_majority = File( filename_majority );
-	auto file_mean = File( filename_mean );
 	auto file_test_error_majority = File( filename_test_error_majority );
 	auto file_test_error_mean = File( filename_test_error_mean );
 	auto file_training = File( filename_training );
 
-	// auto output_name = args[1] ~ ".tex";
-	// auto output = File( output_name, "w" );
-	
-	auto file_majority_lines = file_majority.byLine();
-	auto file_mean_lines = file_mean.byLine();
 	auto file_test_error_majority_lines = file_test_error_majority.byLine();
 	auto file_test_error_mean_lines = file_test_error_mean.byLine();
 	auto file_training_lines = file_training.byLine();
 
 	bool verbose = ( args.length == 2 || args[2] != "sumup" );
-	
-	string[] majority_solutions;
-	string[] mean_solutions;
 	
 	// Training data
 	uint number_success_success_majority; // number of times weak learners were all valid, as well as their majority
@@ -75,75 +63,72 @@ int main( string[] args )
 	uint number_fail_success_mean;
 	uint number_fail_fail_mean;
 
-	uint[] error_training_majority;
-	uint[] error_training_mean;
+	double[] error_training_majority;
+	double[] error_training_mean;
 	
 	double[] wallclock_time;
 	double[] weak_runtimes_all;
 	double[] weak_runtimes_slowest;
 
+	uint success_training_majority;
+	uint success_training_mean;
+	uint[string] success_training_majority_per_solution;
+	uint[string] success_training_mean_per_solution;	
+	uint[string] frequency_training_majority_per_solution;
+	uint[string] frequency_training_mean_per_solution;	
+
 	// Test data
-	uint success_majority;
-	uint success_mean;
-	uint[string] success_majority_per_solution;
-	uint[string] success_mean_per_solution;	
-	uint[string] frequency_majority_per_solution;
-	uint[string] frequency_mean_per_solution;	
+	uint success_test_majority;
+	uint success_test_mean;
+	uint[string] success_test_majority_per_solution;
+	uint[string] success_test_mean_per_solution;	
+	uint[string] frequency_test_majority_per_solution;
+	uint[string] frequency_test_mean_per_solution;	
 
-	foreach( line; file_majority_lines )
-		if( !startsWith( line, "Solution" ) )
-			majority_solutions ~= to!string( line );
+	double[] error_test_majority;
+	double[] error_test_mean;
 
-	foreach( line; file_mean_lines )
-		if( !startsWith( line, "Solution" ) )
-			mean_solutions ~= to!string( line );
-
-	int index = 0;
+	int line_number = 1;
 	foreach( line; file_test_error_majority_lines )
 	{
-		++frequency_majority_per_solution[ majority_solutions[ index ] ];
-
-		if( line == "0" )
+		if( line_number % 2 == 1 ) // lines with training error
+			error_test_majority ~= to!double( line );
+		else // lines with a Q-matrix representation
 		{
-			++success_majority;
-			++success_majority_per_solution[ majority_solutions[ index ] ];
+			string linestring = to!string(line);
+			++frequency_test_majority_per_solution[linestring];
+			if( error_test_majority[$-1] == 0 )
+			{
+				++success_test_majority;
+				++success_test_majority_per_solution[linestring];
+			}
 		}
 		
-		++index;
+		++line_number;
 	}
 
-	index = 0;
+	line_number = 1;
 	foreach( line; file_test_error_mean_lines )
 	{
-		++frequency_mean_per_solution[ mean_solutions[ index ] ];
-
-		if( line == "0" )
+		if( line_number % 2 == 1 ) // lines with test error
+			error_test_mean ~= to!double( line );
+		else // lines with a Q-matrix representation
 		{
-			++success_mean;
-			++success_mean_per_solution[ mean_solutions[ index ] ];
+			string linestring = to!string(line);
+			++frequency_test_mean_per_solution[linestring];
+			if( error_test_mean[$-1] == 0 )
+			{
+				++success_test_mean;
+				++success_test_mean_per_solution[linestring];
+			}
 		}
 		
-		++index;
+		++line_number;
 	}
 
-	if( verbose )
-		writeln("*** Test results ***");
-	writeln("Majority success rate: ", success_majority );
-	writeln("Mean success rate: ", success_mean );
-
-	if( verbose )
-	{
-		writeln();
-		foreach( key, value; success_majority_per_solution )
-			writeln("Majority ", key, " success rate: ", value, "/", frequency_majority_per_solution[key] );
-		
-		writeln();
-		foreach( key, value; success_mean_per_solution )
-			writeln("Mean ", key, " success rate: ", value, "/", frequency_mean_per_solution[key] );
-	}
-	
 	uint count_number_weaks = 0;
 	bool all_success = false;
+	string linestring;
 	foreach( line; file_training_lines )
 	{
 		auto words = line.split(": ");
@@ -155,14 +140,38 @@ int main( string[] args )
 			++count_number_weaks;
 		}
 
+		if( startsWith( line, "Mean solution" ) )
+		{
+			linestring = to!string( words[1] );
+			++frequency_training_mean_per_solution[linestring];
+		}
+
 		if( startsWith( line, "Errors by mean" ) )
 		{
-			error_training_mean ~= to!uint( words[1] );
+			auto error = to!double( words[1] );
+			error_training_mean ~= error;
+			if( error == 0 )
+			{
+				++success_training_mean;
+				++success_training_mean_per_solution[linestring]; // the last value of linestring should correpond to the correct solution
+			}
 		}
 		
+		if( startsWith( line, "Majority solution" ) )
+		{
+			linestring = to!string( words[1] );
+			++frequency_training_majority_per_solution[linestring];
+		}
+
 		if( startsWith( line, "Errors by majority" ) )
 		{
-			error_training_majority ~= to!uint( words[1] );
+			auto error = to!double( words[1] );
+			error_training_majority ~= error;
+			if( error == 0 )
+			{
+				++success_training_majority;
+				++success_training_majority_per_solution[linestring]; // the last value of linestring should correpond to the correct solution
+			}
 		}
 
 		if( startsWith( line, "All weak learners valid" ) )
@@ -211,8 +220,51 @@ int main( string[] args )
 		writefln( "Warning: we do not have 100 mean solutions but ", error_training_mean.length, "." );
 
 	if( verbose )
+		writeln("*** Training results ***");
+	writeln("Majority training success rate: ", success_training_majority );
+	writeln("Mean training success rate: ", success_training_mean );
+	if( verbose )
 	{
-		writeln("\n*** Success/failure situations ***");
+		writeln();
+		foreach( key, value; frequency_training_majority_per_solution )
+		{
+			uint success = 0;
+			if( key in success_training_majority_per_solution )
+				success = success_training_majority_per_solution[key];
+			writeln("Majority ", key, " training success rate: ", success, "/", value );
+		}
+		
+		writeln();
+		foreach( key, value; frequency_training_mean_per_solution )
+		{
+			uint success = 0;
+			if( key in success_training_mean_per_solution )
+				success = success_training_mean_per_solution[key];
+			writeln("Mean ", key, " training success rate: ", success, "/", value );
+		}
+	}
+
+	double mean_error_training_majority = error_training_majority.mean.quantize(0.1);
+	if( verbose )
+		writeln("\n*** Training majority error stats ***");
+	writeln( "Mean training majority error: ", mean_error_training_majority );
+	writeln( "Median training majority error: ", median(error_training_majority).quantize(0.1) );
+	writeln( "Min training majority error: ", error_training_majority.minElement.quantize(0.1) );
+	writeln( "Max training majority error: ", error_training_majority.maxElement.quantize(0.1) );
+	writeln( "Training majority error std dev: ", pop_standard_deviation(error_training_majority).quantize(0.1) );
+
+	double mean_error_training_mean = error_training_mean.mean.quantize(0.1);
+	if( verbose )
+		writeln("\n*** Training mean error stats ***");
+	writeln( "Mean training mean error: ", mean_error_training_mean );
+	writeln( "Median training mean error: ", median(error_training_mean).quantize(0.1) );
+	writeln( "Min training mean error: ", error_training_mean.minElement.quantize(0.1) );
+	writeln( "Max training mean error: ", error_training_mean.maxElement.quantize(0.1) );
+	writeln( "Training mean error std dev: ", pop_standard_deviation(error_training_mean).quantize(0.1) );
+
+	if( verbose )
+	{
+		writeln("\n*** Training success/failure situations ***");
 		writeln( "Number of times weak learners were all valid, as well as their majority: ", number_success_success_majority );
 		writeln( "Number of times weak learners were all valid, but their majority was not: ", number_success_fail_majority );
 		writeln( "Number of times some weak learners were invalid, but their majority was: ", number_fail_success_majority );
@@ -251,14 +303,48 @@ int main( string[] args )
 	writeln( "Max slowest weak learners runtime: ", weak_runtimes_slowest.maxElement.quantize(0.1), "ms" );
 	writeln( "Slowest weak learners std dev: ", pop_standard_deviation(weak_runtimes_slowest).quantize(0.1), "ms" );
 
-// 	output.write("
-// \\begin{table*}[h]\n
-//   \\small\n
-//   \\caption{Success rate over 100 runs on test sets. Timeout is fixed at 1s for each run.}\n
-//   \\begin{center}\n
-//      %\\rowcolors{1}{}{LightCyan}
-//     \\begin{tabular}{@{}|c|c|c|c|c|c|@{}}
-// ");
-	
+	if( verbose )
+		writeln("\n\n*** Test results ***");
+	writeln("Majority test success rate: ", success_test_majority );
+	writeln("Mean test success rate: ", success_test_mean );
+	if( verbose )
+	{
+		writeln();
+		foreach( key, value; frequency_test_majority_per_solution )
+		{
+			uint success = 0;
+			if( key in success_test_majority_per_solution )
+				success = success_test_majority_per_solution[key];
+			writeln("Majority ", key, " test success rate: ", success, "/", value );
+		}
+		
+		writeln();
+		foreach( key, value; frequency_test_mean_per_solution )
+		{
+			uint success = 0;
+			if( key in success_test_mean_per_solution )
+				success = success_test_mean_per_solution[key];
+			writeln("Mean ", key, " test success rate: ", success, "/", value );
+		}
+	}
+
+	double mean_error_test_majority = error_test_majority.mean.quantize(0.1);
+	if( verbose )
+		writeln("\n*** Test majority error stats ***");
+	writeln( "Mean test majority error: ", mean_error_test_majority );
+	writeln( "Median test majority error: ", median(error_test_majority).quantize(0.1) );
+	writeln( "Min test majority error: ", error_test_majority.minElement.quantize(0.1) );
+	writeln( "Max test majority error: ", error_test_majority.maxElement.quantize(0.1) );
+	writeln( "Test majority error std dev: ", pop_standard_deviation(error_test_majority).quantize(0.1) );
+
+	double mean_error_test_mean = error_test_mean.mean.quantize(0.1);
+	if( verbose )
+		writeln("\n*** Test mean error stats ***");
+	writeln( "Mean test mean error: ", mean_error_test_mean );
+	writeln( "Median test mean error: ", median(error_test_mean).quantize(0.1) );
+	writeln( "Min test mean error: ", error_test_mean.minElement.quantize(0.1) );
+	writeln( "Max test mean error: ", error_test_mean.maxElement.quantize(0.1) );
+	writeln( "Test mean error std dev: ", pop_standard_deviation(error_test_mean).quantize(0.1) );
+
 	return 0;
 }
--- results/weak_learners/analyse_results_without_mean.d
+++ results/weak_learners/analyse_results_without_mean.d
@@ -38,70 +38,62 @@ int main( string[] args )
 		return 1;
 	}
 
-	auto filename_majority = args[1] ~ "-majority";
 	auto filename_test_error_majority = args[1] ~ "-test_error_majority";
 	auto filename_training = args[1] ~ "-training";
 	
-	auto file_majority = File( filename_majority );
 	auto file_test_error_majority = File( filename_test_error_majority );
 	auto file_training = File( filename_training );
 
-	auto file_majority_lines = file_majority.byLine();
 	auto file_test_error_majority_lines = file_test_error_majority.byLine();
 	auto file_training_lines = file_training.byLine();
 
 	bool verbose = ( args.length == 2 || args[2] != "sumup" );
-	
-	string[] majority_solutions;
 	
 	// Training data
 	uint number_success_success_majority; // number of times weak learners were all valid, as well as their majority
 	uint number_success_fail_majority; // number of times weak learners were all valid, but their majority was not
 	uint number_fail_success_majority; // number of times some weak learners were not valid, but their majority was
 	uint number_fail_fail_majority; // number of times some weak learners were not valid, as well as their majority
-	
-	uint[] error_training_majority;
+
+	double[] error_training_majority;
 	
 	double[] wallclock_time;
 	double[] weak_runtimes_all;
 	double[] weak_runtimes_slowest;
 
+	uint success_training_majority;
+	uint[string] success_training_majority_per_solution;
+	uint[string] frequency_training_majority_per_solution;
+
 	// Test data
-	uint success_majority;
-	uint[string] success_majority_per_solution;
-	uint[string] frequency_majority_per_solution;
+	uint success_test_majority;
+	uint[string] success_test_majority_per_solution;
+	uint[string] frequency_test_majority_per_solution;
 
-	foreach( line; file_majority_lines )
-		if( !startsWith( line, "Solution" ) )
-			majority_solutions ~= to!string( line );
+	double[] error_test_majority;
 
-	int index = 0;
+	int line_number = 1;
 	foreach( line; file_test_error_majority_lines )
 	{
-		++frequency_majority_per_solution[ majority_solutions[ index ] ];
-
-		if( line == "0" )
+		if( line_number % 2 == 1 ) // lines with training error
+			error_test_majority ~= to!double( line );
+		else // lines with a Q-matrix representation
 		{
-			++success_majority;
-			++success_majority_per_solution[ majority_solutions[ index ] ];
+			string linestring = to!string(line);
+			++frequency_test_majority_per_solution[linestring];
+			if( error_test_majority[$-1] == 0 )
+			{
+				++success_test_majority;
+				++success_test_majority_per_solution[linestring];
+			}
 		}
 		
-		++index;
+		++line_number;
 	}
 
-	if( verbose )
-		writeln("*** Test results ***");
-	writeln("Majority success rate: ", success_majority );
-
-	if( verbose )
-	{
-		writeln();
-		foreach( key, value; success_majority_per_solution )
-			writeln("Majority ", key, " success rate: ", value, "/", frequency_majority_per_solution[key] );
-	}
-	
 	uint count_number_weaks = 0;
 	bool all_success = false;
+	string linestring;
 	foreach( line; file_training_lines )
 	{
 		auto words = line.split(": ");
@@ -113,9 +105,21 @@ int main( string[] args )
 			++count_number_weaks;
 		}
 
+		if( startsWith( line, "Majority solution" ) )
+		{
+			linestring = to!string( words[1] );
+			++frequency_training_majority_per_solution[linestring];
+		}
+
 		if( startsWith( line, "Errors by majority" ) )
 		{
-			error_training_majority ~= to!uint( words[1] );
+			auto error = to!double( words[1] );
+			error_training_majority ~= error;
+			if( error == 0 )
+			{
+				++success_training_majority;
+				++success_training_majority_per_solution[linestring]; // the last value of linestring should correpond to the correct solution
+			}
 		}
 
 		if( startsWith( line, "All weak learners valid" ) )
@@ -152,8 +156,32 @@ int main( string[] args )
 		writefln( "Warning: we do not have 100 majority solutions but ", error_training_majority.length, "." );
 
 	if( verbose )
+		writeln("*** Training results ***");
+	writeln("Majority training success rate: ", success_training_majority );
+	if( verbose )
 	{
-		writeln("\n*** Success/failure situations ***");
+		writeln();
+		foreach( key, value; frequency_training_majority_per_solution )
+		{
+			uint success = 0;
+			if( key in success_training_majority_per_solution )
+				success = success_training_majority_per_solution[key];
+			writeln("Majority ", key, " training success rate: ", success, "/", value );
+		}
+	}
+
+	double mean_error_training_majority = error_training_majority.mean.quantize(0.1);
+	if( verbose )
+		writeln("\n*** Training majority error stats ***");
+	writeln( "Mean training majority error: ", mean_error_training_majority );
+	writeln( "Median training majority error: ", median(error_training_majority).quantize(0.1) );
+	writeln( "Min training majority error: ", error_training_majority.minElement.quantize(0.1) );
+	writeln( "Max training majority error: ", error_training_majority.maxElement.quantize(0.1) );
+	writeln( "Training majority error std dev: ", pop_standard_deviation(error_training_majority).quantize(0.1) );
+
+	if( verbose )
+	{
+		writeln("\n*** Training success/failure situations ***");
 		writeln( "Number of times weak learners were all valid, as well as their majority: ", number_success_success_majority );
 		writeln( "Number of times weak learners were all valid, but their majority was not: ", number_success_fail_majority );
 		writeln( "Number of times some weak learners were invalid, but their majority was: ", number_fail_success_majority );
@@ -186,6 +214,30 @@ int main( string[] args )
 	writeln( "Min slowest weak learners runtime: ", weak_runtimes_slowest.minElement.quantize(0.1), "ms" );
 	writeln( "Max slowest weak learners runtime: ", weak_runtimes_slowest.maxElement.quantize(0.1), "ms" );
 	writeln( "Slowest weak learners std dev: ", pop_standard_deviation(weak_runtimes_slowest).quantize(0.1), "ms" );
-	
+
+	if( verbose )
+		writeln("\n\n*** Test results ***");
+	writeln("Majority test success rate: ", success_test_majority );
+	if( verbose )
+	{
+		writeln();
+		foreach( key, value; frequency_test_majority_per_solution )
+		{
+			uint success = 0;
+			if( key in success_test_majority_per_solution )
+				success = success_test_majority_per_solution[key];
+			writeln("Majority ", key, " test success rate: ", success, "/", value );
+		}
+	}
+
+	double mean_error_test_majority = error_test_majority.mean.quantize(0.1);
+	if( verbose )
+		writeln("\n*** Test majority error stats ***");
+	writeln( "Mean test majority error: ", mean_error_test_majority );
+	writeln( "Median test majority error: ", median(error_test_majority).quantize(0.1) );
+	writeln( "Min test majority error: ", error_test_majority.minElement.quantize(0.1) );
+	writeln( "Max test majority error: ", error_test_majority.maxElement.quantize(0.1) );
+	writeln( "Test majority error std dev: ", pop_standard_deviation(error_test_majority).quantize(0.1) );
+
 	return 0;
 }
